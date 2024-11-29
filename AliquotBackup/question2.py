5ec47e4b09292b5b4bf11f03b96d63285ef2c135
HaroldRobson <wt23188@bristol.ac.uk>
2024-11-29 22:43:50 +0000

    Added Joseph's code for making an Aliquot sequence having imported S

--- AliquotBackup/question2.py
+++ AliquotBackup/question2.py
@@ -1,23 +1,19 @@
-from question1 import S, SOE
+# Joseph's way of making an aliquot sequence
+
+from question1 import S
+
 def A(k):
-    SOE(k * 5)
-    if k < 1:
-        print("Error: no aliquot sequence exists for this input")
-        return None
     n = 30
     i = 10**9
     sequence = [k]
-    for j in range(n-1):
-        if k == S(k):
+    for j in range(1,n+1):
+        if k == S(k) or k > i:
+            break
+        elif k == 0:
+            sequence.append(k)
             break
         else:
             k = S(k)
             sequence.append(k)
-            if k == 0:
-                sequence.append(k)
-                break
-            else:
-                if k > i:
-                    break
     return sequence
-print(A(200))
+print(A(2500))
