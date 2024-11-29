8ccf4171a7d65f49af7f60213bc832b5530afb92
HaroldRobson <wt23188@bristol.ac.uk>
2024-11-29 23:49:30 +0000

    rewrote Joseph's code so it actually works and made it return the loop not just its length

--- AliquotBackup/extension1.py
+++ AliquotBackup/extension1.py
@@ -1,26 +1,21 @@
 import json
+
+"""
+Made this to find the largest loop length from our json file and then return the loop length along with the 
+loop itself. works v quickly - H
+"""
+
 with open('behaviour20000.json', 'r') as file:
     data = json.load(file)
 
 from itertools import islice
-
-
-    
-def max_loop_length(n):
+def max_loop_length(n: int):
     loop_lengths = []
-    loop_dict = {}
-    if not isinstance(n, int) or not (1 <= n <= 19999):
-        print("Input error")
-        return None
-    else:
-        loop_lengths = []
-        for key, value in islice(data.items(), n):
-            length = len(value[1])
-            loop_lengths.append(length)
-            loop_dict[length] = value[1] 
-        if loop_lengths == []:
-            print("No loops detected")
-            return None
-        else:
-            return max(loop_lengths), loop_dict[max(loop_lengths)]
-print(max_loop_length(19999))        
+    loop_lengths_dict = {}
+    for key, value in islice(data.items(), n):
+        if value[0] == "Loop Detected":
+            loop_lengths.append(len(value[1]))
+            loop_lengths_dict[len(value[1])] = value[1]
+    return max(loop_lengths), loop_lengths_dict[max(loop_lengths)]
+print(max_loop_length(20000))
+                              
