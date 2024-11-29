479e4c6d4c13f394bded727de952a511548515a4
HaroldRobson <wt23188@bristol.ac.uk>
2024-11-29 18:40:03 +0000

    Delete question_1_extension_max_loop_length.py

--- question_1_extension_max_loop_length.py
+++
@@ -1,8 +0,0 @@
-def max_loop_length(n):
-    loop_lengths = []
-    for i in json_file:
-        if i[0] == "Loop Detected":
-            c = len(i[1])
-            loop_lengths.append(c)
-    max_length = max(loop_lengths)
-    return max_length
